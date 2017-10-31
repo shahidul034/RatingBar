3cae5be5ebb2e42ec4a03d2efe0594f858fbeda4
SHAKIB <shakibkhanrock@gmail.com>
2017-10-31 18:52:53 +0600

    first commit

---
+++ app/src/main/java/com/shakibcsekuet/ratingbar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.shakibcsekuet.ratingbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // initiate rating bar and a button
+        final RatingBar simpleRatingBar = (RatingBar) findViewById(R.id.simpleRatingBar);
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        // perform click event on button
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get values and then displayed in a toast
+                String totalStars = "Total Stars:: " + simpleRatingBar.getNumStars();
+                String rating = "Rating :: " + simpleRatingBar.getRating();
+                Toast.makeText(getApplicationContext(), totalStars + "\n" + rating, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
